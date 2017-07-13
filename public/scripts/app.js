9b8dd180b773f9466b51233d3e07b0667a6e31a9
Bryce <bryce.sayers-kwan@live.ca>
2017-07-13 22:52:00 +0000

    Impletemented MongoDB. Database now stores the data rather than an in memory object. Cleaned up code. Deleted unnecessary files.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,35 +1,39 @@
 $().ready(function() {
+
   // Malicious text filter
   function escape(str) {
-  var div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   }
+
   // Get the amount of time since the tweet was posted
   function timeAgo(userTime) {
-    var time = ((Date.now() - userTime) / 1000) - 655;
+    const timeDelay = 445; // Vagrant Date.now() delay as of 07/13/2017
+    const time = ((Date.now() - userTime) / 1000) - timeDelay;
     const minute = 60;
     const hour = 3600;
     const day = 86400;
     const year = 31536000;
     switch(true) {
-      case time < minute:
-        return `Posted ${Math.floor(time)} seconds ago`;
-        break;
-      case time >= minute && time < hour:
-        return `Posted ${Math.floor(time / minute)} minute\(s\) ago`;
-        break;
-      case time >= hour && time < day:
-        return `Posted ${Math.floor(time / hour)} hour\(s\) ago`;
-        break;
-      case time >= day && time < year:
-        return `Posted ${Math.floor(time / day)} day\(s\) ago`;
-        break;
-      default:
-        return `Posted over ${Math.floor(time / year)} year\(s\) ago`;
-        break;
-   }
+    case time < minute:
+      return `Posted ${Math.floor(time)} seconds ago`;
+      break;
+    case time >= minute && time < hour:
+      return `Posted ${Math.floor(time / minute)} minute\(s\) ago`;
+      break;
+    case time >= hour && time < day:
+      return `Posted ${Math.floor(time / hour)} hour\(s\) ago`;
+      break;
+    case time >= day && time < year:
+      return `Posted ${Math.floor(time / day)} day\(s\) ago`;
+      break;
+    default:
+      return `Posted over ${Math.floor(time / year)} year\(s\) ago`;
+      break;
+    }
   }
+
   // Create a new tweet format
   function createTweetElement(tweetData) {
     const time = timeAgo(tweetData.created_at);
@@ -37,9 +41,9 @@ $().ready(function() {
     <section class="tweet-box">
        <article class="tweet">
           <header>
-              <img class="profile-photo" src=${tweetData.user.avatars.small}>
-              <span class="name">${tweetData.user.name}</span>
-              <span class="handle">${tweetData.user.handle}</span>
+              <img class="profile-photo" src="${escape(tweetData.user.avatars.small)}">
+              <span class="name">${escape(tweetData.user.name)}</span>
+              <span class="handle">${escape(tweetData.user.handle)}</span>
           </header>
           <div class="box">
             <p>${escape(tweetData.content.text)}</p>
@@ -55,9 +59,9 @@ $().ready(function() {
         </article>
       </section>
       <br>
-      `
+      `;
     return html;
-   }
+  }
 
   // Render the tweets from the object memory database
   function renderTweets(arr) {
@@ -66,35 +70,36 @@ $().ready(function() {
     });
   }
 
-  // Handles the new tweet post
-  function handleNewTweet(event) {
-    event.preventDefault();
-
-    if ($('textarea').val().length > 140) {
-      alert('Too many characters');
-    } else {
-      var $form = $(this);
-      $.post({
-        url: '/tweets',
-        data: $form.serialize()
-      })
-      .done(function() {
-        loadTweets();
-        $('form textarea').val('');
-      })
-      .error(function() {
-        alert('Empty post not supported');
-      });
-    }
-  }
   // Load the tweets from the object memory database
   function loadTweets() {
     $.get({
       url: '/tweets'
     })
-    .done(function(data) {
-      renderTweets(data);
-    });
+      .done(function(data) {
+        renderTweets(data);
+      });
+  }
+
+  // Handles the new tweet post
+  function handleNewTweet(event) {
+    event.preventDefault();
+
+    if ($('textarea').val().length > 140) {
+      alert('You have exceeded the allowed character count.');
+    } else {
+      let $form = $(this);
+      $.post({
+        url: '/tweets',
+        data: $form.serialize()
+      })
+        .done(function() {
+          loadTweets();
+          $('form textarea').val('');
+        })
+        .error(function() {
+          alert('Post not supported.');
+        });
+    }
   }
 
   loadTweets();
